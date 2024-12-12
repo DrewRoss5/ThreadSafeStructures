fdf6bdf4398da3f6bf68030e7bc36482c0c08adf
Drew <145530440+DrewRoss5@users.noreply.github.com>
2024-12-12 15:04:07 -0700

    Use larger datasets
    The idea here is that a larger dataset will trigger reallocations, testing the reallocation functions

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,21 +1,26 @@
+#include <iostream>
+#include <gtest/gtest.h>
 #include "../src/dynarr.hpp"
 #include "../src/stack.hpp"
-#include <gtest/gtest.h>
 
+// test the basic functionality of the DynArr class
 TEST(BasicTests, DynArrBasic){
     DynArr<int> arr;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 1000; i++){
         arr.pushBack(i);
-    for (int i = 0; i < 10; i++)
+    }
+    for (int i = 0; i < 1000; i++)
         ASSERT_EQ(arr[i], i);
 }
 
+// test the basic functionality of the Stack class
 TEST(BasicTests, StackBasic){
     Stack<int> stack;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 1024; i++){
         stack.push(i);
-    int target = 9;
-    while (stack.size()){
+    }
+    int target = 1023;
+    while (target > -1){
         ASSERT_EQ(stack.pop(), target);
         target--;
     }
