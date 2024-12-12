6de63b378658e87949291f26615bab27fb303474
Drew <145530440+DrewRoss5@users.noreply.github.com>
2024-12-12 16:38:09 -0700

    Implement thread safety tests for DynArr

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
+#include <thread>
+#include <algorithm>
+#include <vector>
 #include <gtest/gtest.h>
 #include "../src/dynarr.hpp"
 #include "../src/stack.hpp"
 
+DynArr<int> globalArr;
+
+// populates a DynArr with all integers within the range [start, end)
+void populateArr(int start, int end){
+    for (int i = start; i < end; i++)
+        globalArr.pushBack(i);
+}
+
 // test the basic functionality of the DynArr class
 TEST(BasicTests, DynArrBasic){
     DynArr<int> arr;
-    for (int i = 0; i < 1000; i++){
+    for (int i = 0; i < 1000; i++)
         arr.pushBack(i);
-    }
+    ASSERT_EQ(arr.size(), 1000);
     for (int i = 0; i < 1000; i++)
         ASSERT_EQ(arr[i], i);
 }
@@ -16,9 +27,8 @@ TEST(BasicTests, DynArrBasic){
 // test the basic functionality of the Stack class
 TEST(BasicTests, StackBasic){
     Stack<int> stack;
-    for (int i = 0; i < 1024; i++){
+    for (int i = 0; i < 1024; i++)
         stack.push(i);
-    }
     int target = 1023;
     while (target > -1){
         ASSERT_EQ(stack.pop(), target);
@@ -26,6 +36,27 @@ TEST(BasicTests, StackBasic){
     }
 }
 
+// check for race conditions in the DynArr class
+TEST(ThreadSafetyTests, DynArrSafety){
+    int threadCount = std::thread::hardware_concurrency();
+    std::vector<std::thread> threads;
+    // place 1000 elements from each thread, making a race condition likely
+    for (int i = 0; i < threadCount; i++)
+        threads.emplace_back(populateArr, i * 1000, (i+1) * 1000);
+    for (int i = 0; i < threadCount; i++)
+        threads[i].join();
+    // check that each element in the array occurs exactly once, if any doesn't appear that means it was overwritten via race condition
+    bool safe {true};
+    for (int i = 0; i < threadCount * 1000; i++){
+        unsigned count = std::count_if(&globalArr[0], &globalArr[1000*threadCount], [i] (int x) {return x == i;});
+        if (count != 1){
+            safe = false;
+            break;
+        }
+    }
+    ASSERT_TRUE(safe);
+}
+  
 int main(int argc, char** argv){
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
